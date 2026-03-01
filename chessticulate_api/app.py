d4e3d523a02747b00b56a77410f72c1a5ef26ece
Brian Glaws <glaws.brian@gmail.com>
2026-03-01 12:20:56 -0500

    update version, fix lint and format errors

--- chessticulate_api/app.py
+++ chessticulate_api/app.py
@@ -7,12 +7,12 @@ from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
+from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from chessticulate_api import crud, db, models, routers, schemas
 from chessticulate_api.config import CONFIG
-from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
 
 @asynccontextmanager
@@ -89,6 +89,8 @@ async def signup(
 
     return schemas.GetOwnUserResponse(**vars(user))
 
+
 @app.get("/metrics")
 def metrics():
+    """Prometheus metrics for Grafana dashboards"""
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
